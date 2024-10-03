b5e8bb2fcb88a7f49fa6434126a0ea73f3d639da
Dev-Lucas-Dias <diaslucasaparecido@gmail.com>
2024-10-03 15:56:55 -0300

    Menu com javascript

---
+++ javascript/event.js
@@ -0,0 +1,26 @@
+function lista(){
+let listar =document.getElementById("list");
+listar.style.display="flex";
+
+}
+function fecha(){
+let fechar =document.getElementById("list");
+fechar.style.display="none";
+}
+function list(){
+    let lista =document.getElementById("list1");
+    lista.style.display="flex";
+    }
+    function fech(){
+    let fecha =document.getElementById("list1");
+    fecha.style.display="none";
+    }
+    function menu(){
+        let menubar =document.getElementById("listamenu");
+        menubar.style.display="flex";
+        
+        }
+        function menufech(){
+        let menufech =document.getElementById("listamenu");
+        menufech.style.display="none";
+        }
