c9fbea7c9126c6e43e58c0123bec49376a19d7b9
Dev-Lucas-Dias <diaslucasaparecido@gmail.com>
2024-10-04 10:20:44 -0300

    header limpo

--- javascript/event.js
+++ javascript/event.js
@@ -1,3 +1,5 @@
+//SERVIÇOS
+
 function lista(){
 let listar =document.getElementById("list");
 listar.style.display="flex";
@@ -7,20 +9,32 @@ function fecha(){
 let fechar =document.getElementById("list");
 fechar.style.display="none";
 }
+
+//produtos
+
 function list(){
     let lista =document.getElementById("list1");
     lista.style.display="flex";
+    document.getElementById("img").style.marginTop="250px";
     }
     function fech(){
     let fecha =document.getElementById("list1");
     fecha.style.display="none";
+    
+    
     }
+
+    //click menu
+
     function menu(){
         let menubar =document.getElementById("listamenu");
         menubar.style.display="flex";
-        
-        }
-        function menufech(){
-        let menufech =document.getElementById("listamenu");
-        menufech.style.display="none";
-        }
+        document.getElementById("close").style.display="flex";
+        document.getElementById("menu").style.display="none";
+    }
+    function x(){
+       let menufecha=document.getElementById("listamenu");
+        menufecha.style.display="none";
+        document.getElementById("close").style.display="none";
+        document.getElementById("menu").style.display="flex";
+    }
